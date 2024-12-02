eeeab1ea2bf1f4d41e32e447fe926cc4dd0cc44b
walkervalentinuss@gmail.com <walkervalentinuss@gmail.com>
2024-12-02 13:49:02 +0700

    first commit

---
+++ app/Http/Controllers/MatkulController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Matkul;
+use Illuminate\Http\Request;
+
+class MatkulController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Matkul $matkul)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Matkul $matkul)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Matkul $matkul)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Matkul $matkul)
+    {
+        //
+    }
+}
